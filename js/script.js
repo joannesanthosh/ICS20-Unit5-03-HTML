539f28db9877bfc9e8e3a4cdf852255f6403cc40
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-04-28 17:30:16 +0000

    change 3 files

--- js/script.js
+++ js/script.js
@@ -15,18 +15,17 @@ if (navigator.serviceWorker) {
 }
 
 /**
- * This function tells user what movies they can watch
+ * Input
  */
 function myButtonClicked() {
-  // input
   const age = parseInt(document.getElementById("age").value)
 
   // process
-  if (age > = 17) {
+  if (age > == 17) {
     console.log("You can see an R rated movie alone.");
-  } else if (age > = 13) {
+  } else if (age > == 13) {
     console.log("You can see a PG-13 movie alone.");
-  } else if (age > = 5) {
+  } else if (age > == 5) {
     console.log("You can see a G or PG movie alone.");
   } else {
     console.log("You're too young to watch most things.");
